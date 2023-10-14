b2202dec47c68d3615d8ccda76156ced676b5ca4
markkush-24 <mark_kush@mail.ru>
2023-10-14 19:47:30 +0300

    add rds datasource

--- src/main/java/com/example/stockexchange/configuration/ClientConfiguration.java
+++ src/main/java/com/example/stockexchange/configuration/ClientConfiguration.java
@@ -1,15 +1,9 @@
 package com.example.stockexchange.configuration;
 
 
-import feign.okhttp.OkHttpClient;
-import io.r2dbc.spi.ConnectionFactories;
-import io.r2dbc.spi.ConnectionFactory;
-import io.r2dbc.spi.ConnectionFactoryOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import static io.r2dbc.spi.ConnectionFactoryOptions.*;
 
 @Configuration
 public class ClientConfiguration {
@@ -17,23 +11,5 @@ public class ClientConfiguration {
     @Bean
     public WebClient webClient(){
         return WebClient.builder().build();
-    }
-
-    @Bean
-    public OkHttpClient client() {
-        return new OkHttpClient();
-    }
-
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        return ConnectionFactories.get(
-                ConnectionFactoryOptions.builder()
-                        .option(DRIVER, "postgresql")
-                        .option(HOST, "stockexchange-db")
-                        .option(USER, "postgres")
-                        .option(PASSWORD, "superuser")
-                        .option(DATABASE, "postgres")
-                        .option(PORT, 5432)
-                        .build());
     }
 }
