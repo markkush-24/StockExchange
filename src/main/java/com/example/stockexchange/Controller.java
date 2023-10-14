f9657d4c99a556fc50908e88bbdc4df6f48c7c71
markkush-24 <mark_kush@mail.ru>
2023-10-14 20:33:32 +0300

    add test controller

---
+++ src/main/java/com/example/stockexchange/Controller.java
@@ -0,0 +1,15 @@
+package com.example.stockexchange;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class Controller {
+
+    @GetMapping("hello")
+    public String testMethod() {
+        return "test controller is work";
+    }
+}
